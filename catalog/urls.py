3f09508ff7195a69071454321bfd3a82fb547a2a
xaj <974065907@qq.com>
2020-10-01 13:56:10 +0800

    done renew-book using a form and function view

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     re_path('^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('borrowed/', views.AllLoanedBooksListView.as_view(), name='all-borrowed'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
